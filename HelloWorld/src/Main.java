e3db3d972f6d0fa6791416bccb56fd0a7d9eba91
wxj <3337183289@qq.com>
2019-09-02 16:49:39 +0800

    提交到远程仓库

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Idea第二次提交");
+        System.out.println("第一次克隆修改");
     }
 }
