155abb31814b8b6bc7fa5c09699cbe47574f7db8
wxj1997 <32233648+wxj1997@users.noreply.github.com>
2019-09-02 16:56:14 +0800

    Update Main.java

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("Idea第二次提交");
         System.out.println("第一次克隆修改");
+        System.out.println("第二次克隆修改");
     }
 }
